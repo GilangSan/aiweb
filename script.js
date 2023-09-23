917dea728225ac0098cf2dc60b4e8add176e7f04
Lang <73976981+GilangSan@users.noreply.github.com>
2023-09-23 16:14:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -22,10 +22,22 @@ async function ai(){
     ttt.textContent='Tunggu...'
     el.appendChild(ttt)
   try{
-    let api = await fetch(`https://api.akuari.my.id/ai/gpt?chat=${msg.value}`)
+    let api = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer 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`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            model: 'gpt-4',
+            messages: msg.value,
+            max_tokens: 100,
+        }),
+    })
+    let data = await api.json()
+    const botAnswer = data?.choices?.[0]?.message?.content
     msg.value = ''
-    let apis = await api.json()
-    ttt.textContent=apis.respon
+    ttt.textContent=botAnswer
     canCmd = true
   } catch(error){
     console.log(error)
